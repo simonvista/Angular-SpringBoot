816295b4f4d7895ee15c84d73f31b8675b467db9
YL <simon-ling@hotmail.com>
2021-07-07 21:50:45 -0400

    14314 is done

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,11 @@
+import { CreateEmployeeComponent } from './create-employee/create-employee.component';
 import { EmployeeListComponent } from './employee-list/employee-list.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
   { path: 'employees', component: EmployeeListComponent },
+  { path: 'create-employee', component: CreateEmployeeComponent },
   { path: '', redirectTo: 'employees', pathMatch: 'full' },
 ];
 
