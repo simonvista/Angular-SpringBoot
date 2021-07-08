a2a56ffdd6b8d8536725462975569de56fcd4ebd
YL <simon-ling@hotmail.com>
2021-07-07 20:27:27 -0400

    12430 is done

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,14 @@
+import { EmployeeListComponent } from './employee-list/employee-list.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'employees', component: EmployeeListComponent },
+  { path: '', redirectTo: 'employees', pathMatch: 'full' },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
