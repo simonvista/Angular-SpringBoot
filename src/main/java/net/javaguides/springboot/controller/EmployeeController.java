b53aea19ba4533695e888f624861bebd4a05c488
YL <simon-ling@hotmail.com>
2021-07-08 17:21:40 -0400

    24331 is done

--- src/main/java/net/javaguides/springboot/controller/EmployeeController.java
+++ src/main/java/net/javaguides/springboot/controller/EmployeeController.java
@@ -1,10 +1,13 @@
 package net.javaguides.springboot.controller;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -49,5 +52,14 @@ public class EmployeeController {
 		emp.setEmailId(emp1.getEmailId());
 		Employee updatedEmp=employeeRepository.save(emp);
 		return ResponseEntity.ok(updatedEmp);
+	}
+//	delete employee
+	@DeleteMapping("employees/{id}")
+	public ResponseEntity<Map<String,Boolean>> deleteEmployee(@PathVariable Long id){
+		Employee emp=employeeRepository.findById(id).orElseThrow(()->new ResourceNotFoundException("Employee with the id: "+id+" you provided, does not exist"));
+		employeeRepository.delete(emp);
+		Map<String,Boolean> response=new HashMap<String, Boolean>();
+		response.put("deleted",Boolean.TRUE );
+		return ResponseEntity.ok(response);
 	}
 }
